04a7aea9297a20559c11728a8bc7b722200d8706
Adrian <adrian_zinko@op.pl>
2020-09-20 11:48:16 +0200

    feat: config cra setup

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+/* TODO
+- Install Dependencies 
+- Import dependencies
+- Setup webcam and canvas
+- Load facemash
+- Implement detect function
+- Add drawing utils
+- load triangulation
+- Setup point drawing
+- Add drawMesh to detect function
+*/
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <h1>
+      Facemash
+    </h1>
+  );
+};
+
+export default App;
